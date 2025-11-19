53eecc25070e0534bd702d64a434c0ae9d79f259
Phutarit025 <po3638s@gmail.com>
2025-11-19 15:28:33 +0700

    .

---
+++ home.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.header("TIW")
+st.img("./img/Tiw.jpg")
