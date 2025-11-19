468195d87c9ae3d70f9582f80253cd6928c6e42d
Phutarit025 <po3638s@gmail.com>
2025-11-19 16:24:27 +0700

    Update home.py

--- home.py
+++ home.py
@@ -12,13 +12,13 @@ st.image("./img/Tiw.jpg")
 col1, col2, col3 = st.columns(3)
 with col1:
     st.header("Versicolor")
-    st.image(os.path.join("img", "iris1.jpg"))
+    st.image("./img/iris1.jpg")
 with col2:
     st.header("Verginiga")
-    st.image(os.path.join("img", "iris2.jpg"))
+    st.image("./img/iris2.jpg")
 with col3:
     st.header("Setosa")
-    st.image(os.path.join("img", "iris3.jpg"))
+    st.image("./img/iris3.jpg")
 
 # --- HTML Section สถิติข้อมูล ---
 html_7 = """
