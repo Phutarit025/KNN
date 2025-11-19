2d0d43da5afac42953c3f0c9f289524e3aa8e8cb
Phutarit025 <po3638s@gmail.com>
2025-11-19 16:36:17 +0700

    Update home.py

--- home.py
+++ home.py
@@ -5,79 +5,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-st.header('Tiw')
-st.image("./img/Tiw.jpg")  
+st.title('โปรเจคทำนายชนิดของดอกไอริสด้วย KNN')
+st.subheader('เว็บแอปพลิเคชันสำหรับทำนายสายพันธุ์ดอกไอริส')
+
+st.sidebar.image("./img/Tiw.jpg")
+
+st.markdown("---")
+
+st.write("## สายพันธุ์ดอกไอริส (Iris Species)")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Versicolor")
-   st.image("./img/iris1.jpg")
+   st.subheader("Versicolor")
+   st.image("./img/iris1.jpg", caption="Iris Versicolor")
 
 with col2:
-   st.header("Verginiga")
-   st.image("./img/iris2.jpg")
+   st.subheader("Virginica")
+   st.image("./img/iris2.jpg", caption="Iris Virginica")
 
 with col3:
-   st.header("Setosa")
-   st.image("./img/iris3.jpg")
+   st.subheader("Setosa")
+   st.image("./img/iris3.jpg", caption="Iris Setosa")
 
-html_7 = """
-<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
-</div>
-"""
-st.markdown(html_7, unsafe_allow_html=True)
-st.markdown("")
 
-dt = pd.read_csv("./data/iris.csv")
-st.write(dt.head(10))
 
-dt1 = dt['petallength'].sum()
-dt2 = dt['petalwidth'].sum()
-dt3 = dt['sepallength'].sum()
-dt4 = dt['sepalwidth'].sum()
+# Use caching to load data
+@st.cache_data
+def load_data(path):
+    return pd.read_csv(path)
 
-dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+# Expander for data statistics
+with st.expander("ดูสถิติและกราฟข้อมูลดอกไม้ (View Data Statistics and Chart)"):
+    dt = load_data("./data/iris.csv")
+    
+    st.write("#### ข้อมูล 10 แถวแรก (First 10 rows)")
+    st.write(dt.head(10))
 
-if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   #st.write(dt.head(10))
-   st.bar_chart(dx2)
-   st.button("ไม่แสดงข้อมูล")
-else:
-    st.write("ไม่แสดงข้อมูล")
+    # Prepare data for the chart with meaningful labels
+    feature_sums = dt[['petal.length', 'petal.width', 'sepal.length', 'sepal.width']].sum()
+    sum_df = pd.DataFrame(feature_sums, columns=["ผลรวม (Total Sum)"])
+    
+    st.write("#### กราฟแท่งแสดงผลรวมของแต่ละคุณลักษณะ (Bar Chart of Feature Sums)")
+    st.bar_chart(sum_df)
 
-html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>ทำนายข้อมูล</h5></center>
-</div>
-"""
-st.markdown(html_8, unsafe_allow_html=True)
-st.markdown("")
 
-pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
-pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 
-sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
-sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+@st.cache_resource
+def train_knn_model():
+    dt = load_data("./data/iris.csv")
+    X = dt.drop('variety', axis=1)
+    y = dt.variety
+    Knn_model = KNeighborsClassifier(n_neighbors=3)
+    Knn_model.fit(X, y)
+    return Knn_model
 
-if st.button("ทำนายผล"):
-    #st.write("ทำนาย")
-   dt = pd.read_csv("./data/iris.csv") 
-   X = dt.drop('variety' , axis=1)
-   y = dt.variety   
-   Knn_model = KNeighborsClassifier(n_neighbors=3)
-   Knn_model.fit(X, y)   
-   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
-   st.write(Knn_model.predict(x_input))
-   
-   out=Knn_model.predict(x_input)
+st.markdown("---")
 
-   if out[0] == 'Setosa':
-    st.image("./pic/iris1.jpg")
-   elif out[0] == 'Versicolor':       
-    st.image("./pic/iris2.jpg")
-   else:
-    st.image("./pic/iris3.jpg")
-else:
-    st.write("ไม่ทำนาย")
+with st.expander("ทำนายชนิดของดอกไอริส (Predict Iris Species)"):
+    st.write("### ป้อนค่าเพื่อทำนาย (Enter values for prediction)")
+
+    col_input1, col_input2 = st.columns(2)
+    with col_input1:
+        pt_len = st.slider("ความยาวกลีบดอก (petal.length)", min_value=0.0, max_value=8.0, value=4.0, step=0.1, key='pt_len')
+        pt_wd = st.slider("ความกว้างกลีบดอก (petal.width)", min_value=0.0, max_value=3.0, value=1.5, step=0.1, key='pt_wd')
+    with col_input2:
+        sp_len = st.number_input("ความยาวกลีบเลี้ยง (sepal.length)", min_value=0.0, max_value=8.0, value=5.0, step=0.1, key='sp_len')
+        sp_wd = st.number_input("ความกว้างกลีบเลี้ยง (sepal.width)", min_value=0.0, max_value=5.0, value=3.0, step=0.1, key='sp_wd')
+
+    if st.button("ทำนายผล (Predict)"):
+        Knn_model = train_knn_model()
+        x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+        prediction = Knn_model.predict(x_input)
+        
+        st.subheader(f"ผลการทำนาย: {prediction[0]}")
+        
+        if prediction[0] == 'Setosa':
+            st.image("./img/iris3.jpg", caption=f"Predicted: {prediction[0]}")
+        elif prediction[0] == 'Versicolor':       
+            st.image("./img/iris1.jpg", caption=f"Predicted: {prediction[0]}")
+        else: # Virginica
+            st.image("./img/iris2.jpg", caption=f"Predicted: {prediction[0]}")
