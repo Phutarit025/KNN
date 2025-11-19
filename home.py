67dfa7804eca7d2096af50010729642c83586445
Phutarit025 <po3638s@gmail.com>
2025-11-19 16:04:53 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,37 +2,39 @@ from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
-st.header("Tiw")
-st.image("./img/Tiw.jpg")
-
+st.header('Tiw')
+st.image("./img/Tiw.jpeg")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("Versicolor")
-    st.image("./img/iris1.jpg")
-with col2:
-    st.header("Verginiga")
-    st.image("./img/iris2.jpg")
-with col3:
-    st.header("Setosa")
-    st.image("./img/iris3.jpg")
+   st.header("Versicolor")
+   st.image("./img/iris1.jpg")
 
-    html_7 = """
+with col2:
+   st.header("Verginiga")
+   st.image("./img/iris2.jpg")
+
+with col3:
+   st.header("Setosa")
+   st.image("./img/iris3.jpg")
+
+html_7 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
 </div>
 """
 st.markdown(html_7, unsafe_allow_html=True)
 st.markdown("")
+
 dt = pd.read_csv("./data/iris.csv")
 st.write(dt.head(10))
 
-dt1 = dt['petallength'].sum()
-dt2 = dt['petalwidth'].sum()
-dt3 = dt['sepallength'].sum()
-dt4 = dt['sepalwidth'].sum()
+dt1 = dt['petal.length'].sum()
+dt2 = dt['petal.width'].sum()
+dt3 = dt['sepal.length'].sum()
+dt4 = dt['sepal.width'].sum()
 
 dx = [dt1, dt2, dt3, dt4]
 dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
@@ -59,15 +61,13 @@ sp_len = st.number_input("กรุณาเลือกข้อมูล sepal
 sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
 
 if st.button("ทำนายผล"):
-#st.write("ทำนาย")
-   #dt = pd.read_csv("./data/iris.csv") 
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/iris.csv") 
    X = dt.drop('variety', axis=1)
    y = dt.variety   
    Knn_model = KNeighborsClassifier(n_neighbors=3)
-   Knn_model.fit(X, y)  
-
-   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]]) #ข้อมูล
-
+   Knn_model.fit(X, y)   
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
    st.write(Knn_model.predict(x_input))
    
    out=Knn_model.predict(x_input)
