b1a5410f83b60aaee3a9203b19c7ac184fab1d72
Phutarit025 <po3638s@gmail.com>
2025-11-19 15:39:01 +0700

    hgf

--- home.py
+++ home.py
@@ -6,14 +6,17 @@ import matplotlib.pyplot as plt
 
 st.header("TIW")
 st.image("./img/Tiw.jpg")
-col1, col2, col3 = st.colum(3) 
+
+col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Versicolor")
     st.image("./img/iris1.jpg")
+
 with col2:
-    st.header("Verginiga")
+    st.header("Verginica")
     st.image("./img/iris2.jpg")
+
 with col3:
     st.header("Setosa")
     st.image("./img/iris3.jpg")
